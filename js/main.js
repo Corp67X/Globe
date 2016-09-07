37e134d14b36ad4da7b72aa419ff31f92e8c8d53
RNOVOSELOV <novoselov_roman@mail.ru>
2016-09-07 11:52:45 +0300

    re

--- js/main.js
+++ js/main.js
@@ -36,8 +36,11 @@ jQuery(document).ready(function($) {
     //Fullscreen Background Image Slideshow
     $.backstretch([
         "assets/img/bg/01_home.jpg", 
-        "assets/img/bg/02_about.jpg",
-        "assets/img/bg/03_facts.jpg"
+        "assets/img/bg/02_about.jpg", 
+        "assets/img/bg/03_facts.jpg",
+        "images/_slide_1.jpg",
+        "images/_slide_2.jpg",
+        "images/_slide_3.jpg"
     ], {duration: 3000, fade: 750});
 
 
